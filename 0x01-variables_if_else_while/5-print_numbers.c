aa024a1b9e481fc3a6494d448be7579064a8ec54
“Kent-G93” <kenmwe25@gmail.com>
2023-08-11 13:12:14 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - prints digits of base 10
+  * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; d < 10; i++)
+	{
+		printf("%d", i)
+	}
+	putchar("\n");
+	return (0);
+}
