10f1c2f1976b1a8e05381bd59bcdbe08c634943c
“Kent-G93” <kenmwe25@gmail.com>
2023-08-11 13:22:10 +0300

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - prints base 10 digits without char,printf,puts
+  * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
