38f3afae0ff1ec2c75575bcae38689634c05b292
“Kent-G93” <kenmwe25@gmail.com>
2023-08-15 12:37:14 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+  * main - A program that prints _putchar then newline
+  * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x02. C - Functions, nested loops
+ALX month 0, low level progamming
