471bc326ead027509eb789f55fd562fe50903dac
“Kent-G93” <kenmwe25@gmail.com>
2023-08-17 13:26:21 +0300

    print to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints numbers 0 to 14 , 10 times
+  * Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int count;
+
+	while (count < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar('1');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+		count++;
+	}
+}
