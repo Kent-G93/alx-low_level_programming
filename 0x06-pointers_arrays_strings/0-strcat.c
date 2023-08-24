8ba43606849c83e1250a04acaafc8e363d79cd55
“Kent-G93” <kenmwe25@gmail.com>
2023-08-24 09:47:06 +0300

    new day

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strcat - function that concentrates two strings
+  * @dest: input
+  * @src: input
+  * Return: 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
