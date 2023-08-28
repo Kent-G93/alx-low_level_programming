7dc735c8a2e734d7a6efa43ec2bc73a67857c9c7
“Kent-G93” <kenmwe25@gmail.com>
2023-08-28 10:37:23 +0300

    1st 4 tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _strspn - gets the length of a prefix substring 
+  * _strspn - gets the length of a prefix substring
   * @s: input
   * @accept: input
   * Return: 0
@@ -9,4 +9,27 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int count = 0;
 
+	while (*s != '\0')
+	{
+		int found = 0;
+		char *a;
+
+		for (a = accept; *a != '\0'; a++)
+		{
+			if (s == a)
+			{
+				found = 1;
+				break;
+			}
+			else if (found == 0)
+			{
+				break;
+			}
+		}
+		count++;
+		s++;
+	}
+	return (count);
+}
