280508dc83e85e9dce1f2d7325af4f48743f605d
“Kent-G93” <kenmwe25@gmail.com>
2023-08-28 10:56:38 +0300

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strpbrk - searches a string for any of a set of bytes
+  * @s: input
+  * @accept: input
+  * Return: 0
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+		a++;
+		}
+		s++;
+	}
+	return ("NULL");
+}
