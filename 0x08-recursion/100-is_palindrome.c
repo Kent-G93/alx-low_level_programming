7fdf3f8903c71fd996edf928937abbb2d63c2833
“Kent-G93” <kenmwe25@gmail.com>
2023-08-30 15:37:58 +0300

    advanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,7 +14,7 @@ int is_palindrome(char *s)
 	if (*s == 0)
 	{
 		return (1);
-	}	
+	}
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
@@ -30,7 +30,7 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 	{
 		return (0);
-	}	
+	}
 	return (1 + _strlen_recursion(s + 1));
 }
 
