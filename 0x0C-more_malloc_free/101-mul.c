46d04736f6ddde406321f8ed70890476d842b741
“Kent-G93” <kenmwe25@gmail.com>
2023-09-07 12:53:11 +0300

    more mallocs

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,101 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 
-int is_positive_integer(const char *str);
+#define ERR_MSG "Error"
 
 /**
- * is_positive_integer - finds out if an int is positive
- * @str: int
- * Return: 0 success, 1 not
-*/
-
-int is_positive_integer(const char *str)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	if (*str == '\0')
+	int i = 0;
+
+	while (s[i])
 	{
-		return (0);
-	}
-	while (*str != '\0')
-	{
-		if (!isdigit(*str))
-		{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		}
-		str++;
+		i++;
 	}
 	return (1);
 }
 
 /**
- * main - entry point
- * @argc: count
- * @argv: array
- * Return: 98
-*/
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	int result;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		/*Incorrect number of arguments*/
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2]))
+	for (i = 0; i < len - 1; i++)
 	{
-		printf("Error\n");
-		/*Invalid input, not positive integers*/
-		return (98);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	/*Convert the command-line arguments to integers*/
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	/*Calculate the product*/
-	result = num1 * num2;
-
-	/*Print the result*/
-	printf("%d\n", result);
-
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
