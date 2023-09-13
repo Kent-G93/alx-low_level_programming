18a7a9d9a4f73f892b6bf71b468f2b029b420eac
“Kent-G93” <kenmwe25@gmail.com>
2023-09-13 12:51:02 +0300

    pointer to funct

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints each array element on \n
+ * @array: array
+ * @size: elements
+ * @action: pointer to print
+ * Return: 0
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+
+}
