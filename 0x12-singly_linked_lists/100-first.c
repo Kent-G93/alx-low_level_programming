254027b7315a95bc52757f2e804b88dd5f8f58e9
“Kent-G93” <kenmwe25@gmail.com>
2023-09-21 21:13:37 +0300

    singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * my_constructor - prints before main
+ * Returns: 0
+*/
+
+void my_constructor(void) __attribute__((constructor));
+
+void my_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
