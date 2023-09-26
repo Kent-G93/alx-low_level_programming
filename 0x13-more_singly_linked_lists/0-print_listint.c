d489d99dc2bdcb1820236cf6708243ecc7854657
“Kent-G93” <kenmwe25@gmail.com>
2023-09-26 20:18:00 +0300

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - function to print linked list_t list
+ * @h: head of linked lists
+ * Return: printed chars
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
