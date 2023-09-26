34436ac8e98eb74a266f36bf80aba04fa8df0e79
“Kent-G93” <kenmwe25@gmail.com>
2023-09-26 21:02:33 +0300

    free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,24 +4,21 @@
 /**
  * free_listint2 - function that frees list
  * @head: head of linked lists
- * Return: 0
 */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *temp;
 
-	current = *head;
-
-	if (current == NULL)
-	{
+	if (head == NULL)
 		return;
-	}
-	while (current != NULL)
+
+	while (*head)
 	{
-		temp = current->next;
-		free(current);
-		current = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+
 	*head = NULL;
 }
