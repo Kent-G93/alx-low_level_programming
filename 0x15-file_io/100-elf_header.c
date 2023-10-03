04a30af93d8ebd31ac9104ffb8c372c212062068
“Kent-G93” <kenmwe25@gmail.com>
2023-10-03 22:34:21 +0300

    file i/o 3 cp

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -66,8 +66,10 @@ void print_elf_header_info(Elf64_Ehdr *header)
 	for (i = 0; i < EI_NIDENT; i++)
 	{
 		printf("%02x", header->e_ident[i]);
-
-		if (i < EI_NIDENT - 1) printf(" ");
+		if (i < EI_NIDENT - 1)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 
