e5bab9e614444e1f16283207492d1b45c758a483
“Kent-G93” <kenmwe25@gmail.com>
2023-10-03 19:34:14 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,13 +18,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
 
 	if (text_content != NULL)
 	{
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+		if (fd == -1)
+			return (-1);
 		bytes_written = write(fd, text_content, strlen(text_content));
 		if (bytes_written == -1)
 		{
@@ -33,5 +33,5 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	close(fd);
-	return (-1);
+	return (1);
 }
