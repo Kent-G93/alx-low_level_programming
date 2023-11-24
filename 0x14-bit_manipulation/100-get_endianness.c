1e7ab7afb7ffad5e576455280281d04395b68e04
“Kent-G93” <kenmwe25@gmail.com>
2023-11-24 11:27:11 +0300

    bit manipulation 2

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* get_endianness - Checks the endianness of the system.
+* Return: 0 if big endian, 1 if little endian.
+*/
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+
+	char *byte_ptr = (char *)&num;
+
+	if (*byte_ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
